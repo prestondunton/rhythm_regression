91cf693c5aa5a2f02168a9f23bd36783271429da
prestondunton <preston.dunton@gmail.com>
2022-04-01 22:18:30 -0600

    Added bpm rounding

--- rhythm_regression/midi_processing.py
+++ rhythm_regression/midi_processing.py
@@ -2,6 +2,8 @@ from rhythm_regression.unit_conversion import uspb_to_bpm
 
 import numpy as np
 
+MAX_ROUNDING_ERROR = 0.0013333377777939859  # See MIDI.ipynb for computation.  Assumes a max tempo of 400.
+
 
 def get_midi_vector(midi):
     """
@@ -41,4 +43,8 @@ def get_bpm(midi):
         if message.type == 'set_tempo':
             microseconds_per_beat = message.tempo
             bpm = uspb_to_bpm(microseconds_per_beat)
+
+            if abs(bpm - round(bpm)) < MAX_ROUNDING_ERROR:
+                bpm = round(bpm)
+
             return bpm
